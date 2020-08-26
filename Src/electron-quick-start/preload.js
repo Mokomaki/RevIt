f07f99cf05f57165efc46facf74909e9aaf3c4e8
Mokomaki <grest.hyber@gmail.com>
2020-08-26 18:37:45 +0300

    Made some errors.

--- Src/electron-quick-start/preload.js
+++ Src/electron-quick-start/preload.js
@@ -1,11 +1,23 @@
 // All of the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extension.
+const { remote } = require('electron');
+
+let currWindow = remote.BrowserWindow.getFocusedWindow();
+  
+window.closeCurrentWindow = function()
+{
+  currWindow.close();
+}
+
+
+
+
+
 window.addEventListener('DOMContentLoaded', () => {
   const replaceText = (selector, text) => {
     const element = document.getElementById(selector)
     if (element) element.innerText = text
   }
-
   for (const type of ['chrome', 'node', 'electron']) {
     replaceText(`${type}-version`, process.versions[type])
   }
--- Src/electron-quick-start/renderer.js
+++ Src/electron-quick-start/renderer.js
@@ -4,3 +4,10 @@
 // `nodeIntegration` is turned off. Use `preload.js` to
 // selectively enable features needed in the rendering
 // process.
+
+let closebtn = document.getElementById('win_close_btn');
+
+closebtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  window.closeCurrentWindow();
+});
