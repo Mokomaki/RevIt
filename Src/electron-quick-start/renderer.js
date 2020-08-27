629cd098b275ce005c8d0eb07afe37a18f5044bd
Mokomaki <grest.hyber@gmail.com>
2020-08-27 17:18:01 +0300

    WÄÄÄÄÄÄ*ÄÄÄÄ

--- Src/electron-quick-start/renderer.js
+++ Src/electron-quick-start/renderer.js
@@ -1,13 +1,5 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// No Node.js APIs are available in this process because
-// `nodeIntegration` is turned off. Use `preload.js` to
-// selectively enable features needed in the rendering
-// process.
 
-let closebtn = document.getElementById('win_close_btn');
-
-closebtn.addEventListener('click', (e) => {
-  e.preventDefault();
-  window.closeCurrentWindow();
+document.getElementById("Button").addEventListener('click', function() 
+{
+  window.test();
 });
