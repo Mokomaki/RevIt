c3c3eb575b26647b3bee2d612143acc115607e6b
Mokomaki <grest.hyber@gmail.com>
2020-09-23 15:30:45 +0300

    HAHAAA THE FUCKER WORKS!

--- Src/electron-quick-start/main.js
+++ Src/electron-quick-start/main.js
@@ -1,7 +1,52 @@
-const {app, BrowserWindow, Menu} = require('electron');
+const {app, ipcMain ,BrowserWindow, Menu} = require('electron');
 const path = require('path');
+const sql = require('mysql');
 
 let mainWindow;
+
+ipcMain.on('SEND_QUERY', (event, arg) => 
+{
+  var connection = sql.createConnection(
+    {
+        host     : "185.53.85.170",
+        port     : "3306",
+        user     : "risto_h",
+        password : "dbpasswordisristo",
+        database : "risto_h"
+    }
+  );
+
+  connection.connect((err)=> {
+          if (err) throw err;
+          else
+          {
+              console.log('connected');
+          }
+      }
+  );
+
+  var command = arg;
+  connection.query(command, function (error, results, fields) 
+  {
+    if (error) console.log(error.code);
+    else 
+    {
+      event.reply('REPLY_QUERY', 'pong')
+    }
+  });
+
+
+  connection.end(
+    function(err) {
+        if (err) {
+          return console.log('error:' + err.message);
+        }
+        console.log('connection ended');
+    }
+  );
+  event.reply('asynchronous-reply', 'pong')
+})
+
 
 function createWindow () 
 {
--- Src/electron-quick-start/preload.js
+++ Src/electron-quick-start/preload.js
@@ -1,5 +1,6 @@
 const remote = require('electron').remote;
-const sql = require('mysql');
+const { ipcRenderer } = require('electron')
+
 
 //window controls  
 window.CloseWindow = function() 
@@ -24,44 +25,9 @@ window.MaximizeWindow = function()
 
 window.AddPost = function(authorID, title, contents, categoryID)
 {
-    var connection = sql.createConnection(
-        {
-            host     : "185.53.85.170",
-            port     : "3306",
-            user     : "risto_h",
-            password : "dbpasswordisristo",
-            database : "risto_h"
-        }
-    );
-
-    connection.connect((err)=> {
-            if (err) throw err;
-            else
-            {
-                console.log('connected');
-            }
-        }
-    );
-
     if(authorID!=null&&title!=null&&contents!=null&&categoryID!=null)
     {
-        var command = 'INSERT INTO `posts` (`authorID`,`title`,`contents`,`categoryID`) VALUES ("'+ authorID + '","' + title + '",' +"'" + JSON.stringify(contents) + "'" + ',"' + categoryID + '");'
-        connection.query(command, function (error, results, fields) 
-        {
-            if (error) console.log(error.code);
-            else 
-            {
-                console.log(results);
-            }
-        });
+        var arg = 'INSERT INTO `posts` (`authorID`,`title`,`contents`,`categoryID`) VALUES ("'+ authorID + '","' + title + '",' +"'" + JSON.stringify(contents) + "'" + ',"' + categoryID + '");';
+        ipcRenderer.send('SEND_QUERY', arg)
     }
-
-    connection.end(
-        function(err) {
-            if (err) {
-              return console.log('error:' + err.message);
-            }
-            console.log('connection ended');
-        }
-    );
 }
