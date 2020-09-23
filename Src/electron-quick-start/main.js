5e2deedab8e48f11616f335cec9e08710ba95015
Mokomaki <grest.hyber@gmail.com>
2020-09-24 01:22:52 +0300

    Posting ~works. reading parses html but does not display values.

--- Src/electron-quick-start/main.js
+++ Src/electron-quick-start/main.js
@@ -3,8 +3,14 @@ const path = require('path');
 const sql = require('mysql');
 
 let mainWindow;
+var postPageID;
 
-ipcMain.on('SEND_QUERY', (event, arg) => 
+ipcMain.on('SET_POSTPAGE_ID', function(event,arg)
+{
+  postPageID = arg;
+});
+
+ipcMain.on('GET_CONTENTS_BY_ID', function(event,arg)
 {
   var connection = sql.createConnection(
     {
@@ -16,37 +22,90 @@ ipcMain.on('SEND_QUERY', (event, arg) =>
     }
   );
 
-  connection.connect((err)=> {
-          if (err) throw err;
-          else
-          {
-              console.log('connected');
-          }
-      }
-  );
-
-  var command = arg;
-  connection.query(command, function (error, results, fields) 
+  connection.connect(function(err)
   {
-    if (error) console.log(error.code);
-    else 
+    if (err) throw err;
+    else
     {
-      event.reply('REPLY_QUERY', 'pong')
+        console.log('connected');
     }
   });
 
+  connection.query('SELECT `contents` FROM `risto_h`.`posts` WHERE `postID` = 6', function (error, results, fields) 
+  {
+    if(error) throw error;
+    var dec = new TextDecoder("utf-8");
+    contentObject = Object.assign({}, results);
+    event.reply('GET_CONTENTS_BY_ID_REPLY', dec.decode(contentObject[0].contents));
+    console.log(dec.decode(contentObject[0].contents))
+  });
+/*con.connect(function(err) {
+  if (err) throw err;
+  con.query("SELECT contents FROM risto_h.posts WHERE postID = 5", function (err, result, fields) {
+    if (err) throw err;
+    trivial_object = Object.assign({}, result);
+    dec = new TextDecoder("utf-8");
+    console.log(dec.decode(trivial_object[0].contents));
+  });
+}); */
+  connection.end(function(err) 
+  {
+    if (err) 
+    {
+      return console.log('error:' + err.message);
+    }
+    console.log('connection ended');
+  });
+});
 
-  connection.end(
-    function(err) {
-        if (err) {
-          return console.log('error:' + err.message);
-        }
-        console.log('connection ended');
+ipcMain.on('ADDPOST_SEND_QUERY', (event, arg) => 
+{
+  var command = arg;
+
+  var connection = sql.createConnection(
+    {
+        host     : "185.53.85.170",
+        port     : "3306",
+        user     : "risto_h",
+        password : "dbpasswordisristo",
+        database : "risto_h"
     }
   );
-  event.reply('asynchronous-reply', 'pong')
-})
 
+  connection.connect(function(err)
+  {
+    if (err) throw err;
+    else
+    {
+        console.log('connected');
+    }
+  });
+
+  connection.query(command, function (error, results, fields) 
+  {
+    if (error)
+    {
+      console.log(error.code);
+      event.reply('ADDPOST_REPLY_QUERY', 'failed');
+    } 
+    else 
+    {
+      connection.query('SELECT last_insert_id();', function (error, results, fields) 
+      {
+        event.reply('ADDPOST_REPLY_QUERY', results);
+      });
+    }
+  });
+
+  connection.end(function(err) 
+  {
+    if (err) 
+    {
+      return console.log('error:' + err.message);
+    }
+    console.log('connection ended');
+  });
+});
 
 function createWindow () 
 {
--- Src/electron-quick-start/preload.js
+++ Src/electron-quick-start/preload.js
@@ -1,6 +1,6 @@
 const remote = require('electron').remote;
-const { ipcRenderer } = require('electron')
-
+const { ipcRenderer } = require('electron');
+const Quill = require('quill');
 
 //window controls  
 window.CloseWindow = function() 
@@ -25,9 +25,40 @@ window.MaximizeWindow = function()
 
 window.AddPost = function(authorID, title, contents, categoryID)
 {
-    if(authorID!=null&&title!=null&&contents!=null&&categoryID!=null)
+    if(authorID!=null&&title!=""&&contents!=""&&categoryID!=null)
     {
         var arg = 'INSERT INTO `posts` (`authorID`,`title`,`contents`,`categoryID`) VALUES ("'+ authorID + '","' + title + '",' +"'" + JSON.stringify(contents) + "'" + ',"' + categoryID + '");';
-        ipcRenderer.send('SEND_QUERY', arg)
+        ipcRenderer.send('ADDPOST_SEND_QUERY', arg);
     }
+}
+
+ipcRenderer.on('ADDPOST_REPLY_QUERY', function(event, arg) 
+{
+    console.log(arg)
+    if(arg=="failed")
+    {
+        document.getElementById("error_msg").innerHTML = "An error occurred while sending review...";
+    }
+    else
+    {
+        window.location.href = "readpost.html";
+        ipcRenderer.send('SET_POSTPAGE_ID',arg);
+        //document.getElementById("post_container").innerHTML = convertDeltaToHtml(arg);
+        //console.log(arg);
+    }
+})
+
+window.GetPostContentsById = function()
+{
+    ipcRenderer.send('GET_CONTENTS_BY_ID','foo');
+}
+ipcRenderer.on('GET_CONTENTS_BY_ID_REPLY', function(event, arg) 
+{
+    console.log(arg);
+    document.getElementById("post_container").innerHTML = quillGetHTML(arg);
+})
+function quillGetHTML(inputDelta) {
+    var tempCont = document.createElement("div");
+    (new Quill(tempCont)).setContents(inputDelta);
+    return tempCont.getElementsByClassName("ql-editor")[0].innerHTML;
 }
