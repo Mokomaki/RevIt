bea60dbab8e897a48c89d43b1a7184f8befb74f1
Mokomaki <grest.hyber@gmail.com>
2020-09-30 10:31:20 +0300

    Rebuilt app.

--- Src/electron-quick-start/main.js
+++ Src/electron-quick-start/main.js
@@ -185,7 +185,7 @@ function createWindow ()
 
   mainWindow.loadFile('index.html');
 
-  mainWindow.webContents.openDevTools();
+  //mainWindow.webContents.openDevTools();
 }
 
 app.whenReady().then(() => 
